f6d496ffd4fc4edec06502499aa5c9a5c26785fd
yuhkih <yuhkih@hotmail.com>
2024-02-08 20:11:05 +0900

    test

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "net/http"
+  "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,8 +11,24 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 
+
 func main() {
+
+
+  var port string
+  var ok  bool
+  port, ok = os.LookupEnv("SERVER_PORT")
+  // port = os.Getenv("SERVER_PORT")
+
+  if !ok {
+      fmt.Println("SERVER_PORT is not set")
+      port = "8081"
+  }
+  fmt.Println("SERVER_PORT is " + port)
+  // port = ":8082"
   http.HandleFunc("/", handler) 
-  http.ListenAndServe(":8080", nil)
+  // http.ListenAndServe(":8080", nil)
+  port = ":" + port
+  http.ListenAndServe(port, nil)
 }
 
