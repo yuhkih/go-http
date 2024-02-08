80179bcb0b5545c599936a42a4af6ab5b56f927a
yuhkih <yuhkih@hotmail.com>
2024-02-08 22:26:41 +0900

    test

---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello, Go World")
+}
+
+
+
+func main() {
+
+  var port string
+  var ok  bool
+  port, ok = os.LookupEnv("SERVER_PORT")
+  // port = os.Getenv("SERVER_PORT")
+
+  if !ok {
+      fmt.Println("SERVER_PORT is not set")
+      port = "8080" // default
+  }
+
+  fmt.Println("SERVER_PORT is " + port)
+
+  http.HandleFunc("/", handler)
+  port = ":" + port
+  http.ListenAndServe(port, nil)
+}
+
